540114f1218887a06efdaaf0b04e3a3f10d8af94
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-20 16:10:11 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+// import user from "./data/user";
 
 ReactDOM.render(<App />, document.getElementById("root"));
